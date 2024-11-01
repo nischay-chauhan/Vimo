76d57326034c6ac2dcdda085dc44f54ea8283cdd
nischay-chauhan <masternischay@outlook.com>
2024-11-01 17:52:52 +0530

    starting login functionality

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,6 +1,49 @@
+import { NextResponse } from "next/server";
+import {prisma} from "@/lib/index";
+import { SignJWT } from "jose";
+import { cookies } from "next/headers";
+
+const createToken = async(email : string , userId : number) => {
+    return await new SignJWT({ email, userId , isAdmin : true })
+    .setProtectedHeader({ alg: "HS256" })
+    .setExpirationTime("48h")
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET!));
+}
+
 export async function POST(request: Request) {
+   try{
     const body = await request.json();
     const { email, password } = body;
+    if(!email || !password){
+      return NextResponse.json({message : "Please provide email and password"}  , { status: 400 });
+    }
+
+    const user = await prisma.admin.findUnique({
+      where: {
+        email,
+        password
+      }
+    });
+
+    if(!user){
+        return NextResponse.json({ message: "Invalid email or password" }, { status: 401 });
+    }else{
+        const token = await createToken(user.email, user.id);
+        const cookieStore = await cookies();
+        cookieStore.set("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "lax"
+        })
+    }
+
+
     console.log(email, password);
-    return new Response(JSON.stringify({ message: "Login successful" }));
+    return NextResponse.json({userIndo : {
+        email : user.email,
+        id : user.id,
+    }} );
+   }catch(error){
+    return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+   }
 }
--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,3 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import prisma from "./prisma";
 
-let prisma:PrismaClient;
-
-declare global {
-    var prisma: PrismaClient 
-}
-
-if(process.env.NODE_ENV === "production"){
-    prisma = new PrismaClient();
-}else{
-   if(!global.prisma){
-    global.prisma = new PrismaClient();
-   }
-   prisma = global.prisma;
-}
+export { prisma };
