3b2a9885a524503da2fdf4984b9a05f02fb45858
nischay-chauhan <masternischay@outlook.com>
2024-11-03 12:35:54 +0530

    adding color to the queue

--- components/admin/scraping/scraping-queue.tsx
+++ components/admin/scraping/scraping-queue.tsx
@@ -6,11 +6,23 @@ const ScrapingQueue = () => {
     const [onGoingJobs , setOnGoingJobs] = useState(0)
 
     const onGoingJobsColor = () => {
-
+        if(onGoingJobs <= 5){
+            return "bg-green-500"
+        }else if (onGoingJobs > 5 && onGoingJobs <= 10){
+            return "bg-orange-500"
+        }else{
+            return "bg-red-500"
+        }
     }
 
     const onGoingJobsTextColor = () => {
-
+        if(onGoingJobs <= 5){
+            return "text-green-500"
+        }else if (onGoingJobs > 5 && onGoingJobs <= 10){
+            return "text-orange-500"
+        }else{
+            return "text-red-500"
+        }
     }
 
     useEffect(() => {
@@ -29,9 +41,9 @@ const ScrapingQueue = () => {
     <Card className='h-full'>
         <CardHeader>Current Queue</CardHeader>
         <CardContent className='flex items-center justify-center'>
-            <div className= {`h-52 w-52 rounded-fullflex items-center justify-center ${onGoingJobsColor()}`}>
+            <div className= {`h-52 w-52 rounded-full flex items-center justify-center ${onGoingJobsColor()}`}>
             <div className='h-44 w-44 bg-white rounded-full flex items-center justify-center'>
-                <h4 className='text-6xl font-bold'>
+                <h4 className={`text-6xl font-bold ${onGoingJobsTextColor()}`}>
                     {onGoingJobs}
                 </h4>
 
